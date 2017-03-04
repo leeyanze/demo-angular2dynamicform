34db0dcc3f2fa11ed440bd2b165090e9939ac6cc
Malcolm Lee <leeyanzesg@yahoo.com.sg>
2017-03-04 21:48:32 +0800

    commit test

--- app/demo-charts/demo-charts.component.ts
+++ app/demo-charts/demo-charts.component.ts
@@ -2,21 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
 	template: `
-            <chart [options]="options" (load)="saveInstance($event.context)"></chart>
+            <chart [options]="options" 
+            	(load)="saveInstance($event.context)"></chart>
 		`
 })
 export class DemoChartsComponent {
+	chart: any;
+	options: any;
+	series_data: any;
 	constructor() {
+		this.series_data = [2,3,5,8,13]
         this.options = {
           chart: { type: 'spline' },
-          title: { text : 'dynamic data example'}
-          series: [{ data: [2,3,5,8,13] }]
+          title: { text : 'dynamic data example'},
+          series: [{ data: this.series_data }]
         };
-        setInterval(() => this.chart.series[0].addPoint(Math.random() * 10), 1000);
+        setInterval(() => {
+        	this.series_data.push(Math.random() * 10)
+        	console.log(this.series_data);
+        }, 1000);
     }
-    chart : HighchartsChartObject;
-    options: HighchartsOptions;
-    saveInstance(chartInstance) {
+    saveInstance(chartInstance:any) {
         this.chart = chartInstance;
     }
 }
